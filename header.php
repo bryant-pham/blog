8d859167e32dcbb5ec88bd5ae4162430ed4a3e03
Bryant Pham <bryantphamm@gmail.com>
2014-12-14 06:36:46 +0000

    Added post title/content/date, and styling

--- header.php
+++ header.php
@@ -4,11 +4,13 @@
 	<meta charset="utf-8">
     	<meta http-equiv="X-UA-Compatible" content="IE=edge">
     	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css"/>
+	<link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url'); ?>"/>
 	<script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
 </head>
 <body>
 	<nav class="navbar navbar-default" role="navigation">
-		BRYANT PHAM
+		<div class="container-fluid">
+		</div>
 	</nav>
--- index.php
+++ index.php
@@ -1,4 +1,21 @@
 <?php get_header(); ?>
 
-
+<div class="container-fluid main-content">
+	<div class="row">
+		<div class="col-lg-4"></div>
+		<div class="col-lg-4">
+		<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+			<div class="post-container">
+				<div class="post-title"><?php the_title() ?></div>
+				<div class="post-content"><?php the_content() ?></div>
+				<div class="post-date">
+					<span><?php the_date() ?></span>
+					<span><?php the_time() ?></span>
+				</div> 
+			</div>	
+		<?php endwhile; endif; ?>
+		</div>
+		<div class="col-lg-4"></div>
+	</div>
+</div>
 <?php get_footer(); ?>
